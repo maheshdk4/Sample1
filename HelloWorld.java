a90385870e67376ee7344cd94da34a9fd800db92
maheshdk4 <maheshdk46@gmail.com>
2025-10-09 17:26:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("added 1 more update");
+        System.out.println("added second update");
     }
 }
